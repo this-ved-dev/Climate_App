ce4412252078c29bf276183cd1bf48738564aea6
Vedant Misra <veddev404@gmail.com>
2021-03-14 16:07:59 +0530

    Add files via upload

---
+++ mail_handler.php
@@ -0,0 +1,21 @@
+<!php
+
+if (isset($_POST['submit'])){
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+    $msg = $_POST['message'];
+
+    $to='vedantmisra4444@gmail.com';
+    $subject='Form Submission';
+    $message="Name: ".$name."\n"."Phone: ".$phone."\n"."Wrote the following: "."\n\n".$msg;
+    $headers="From: ".$email;
+
+    if(mail($to,$subject,$message,$headers)){
+        echo"<h1>Sent Successfully! Thank You"." ".$name.", We will contact you shortly!</h1>";
+    }
+    else{
+        echo "Something went wrong!";
+    }
+}
+?>
